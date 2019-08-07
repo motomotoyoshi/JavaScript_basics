8ee87ad4635c508a6eb553fad64f00cb5f251953
motomotoyoshi <motomoto.git@gmail.com>
2019-08-08 00:37:42 +0900

    update, main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('Hello from main.js');
+{
+    class Player {
+        constructor(name, score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        showInfo() {
+            console.log(`name: ${this.name} score: ${this.score}`);
+        }
+    }
+
+    const yura = new Player('yura', 90);
+    const oki = new Player('oki', 80);
+
+    // console.log(yura);
+    yura.showInfo();
+    oki.showInfo();
+}
